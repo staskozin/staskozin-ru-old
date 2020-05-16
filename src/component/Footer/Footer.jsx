1427deb04ccc9e85dd1e0199bcb974f0c1dc55aa
Stanislav Kozin <stas@staskozin.ru>
2020-05-16 23:32:09 +0300

    Update Footer.jsx

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -5,7 +5,11 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <footer className="footer">
-        <p className="footer__left">&copy; 2014&ndash;{new Date().getFullYear()} Станислав Козин</p>
+        <div className="footer__left">
+          <p>&copy; 2014&ndash;{new Date().getFullYear()} Станислав Козин</p>
+          <p>Иконки штук &mdash; <a href="http://sergeychikin.ru/365/">Сергей Чикин</a></p>
+        </div>
+        
         <p className="footer__right">Эл. почта: <a href="mailto:stas@staskozin.ru">stas@staskozin.ru</a></p>
     </footer>
   )
