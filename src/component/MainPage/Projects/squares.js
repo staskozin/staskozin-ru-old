d68d05b7f313f9a358b4a8f5fb1365a62f99b447
Stanislav Kozin <stas@staskozin.ru>
2020-07-11 16:46:07 +0300

    Add link to iphones poster

--- src/component/MainPage/Projects/squares.js
+++ src/component/MainPage/Projects/squares.js
@@ -63,6 +63,7 @@ export default [
     "img": "/img/squares/profshampur.webp"
   },
   {
+    "link": "/files/iphones-poster.pdf",
     "text": "Плакат с размерами айфонов",
     "tags": [
       "граф"
