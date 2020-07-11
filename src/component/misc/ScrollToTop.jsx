24329d8246f58670a599417ed9278cb59cb07867
Stanislav Kozin <stas@staskozin.ru>
2020-07-12 02:17:49 +0300

    Remove old webp polyfill

--- src/component/misc/ScrollToTop.jsx
+++ src/component/misc/ScrollToTop.jsx
@@ -1,15 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import { WebpMachine } from 'webp-hero';
-const webpMachine = new WebpMachine();
-
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    webpMachine.polyfillDocument();
   }, [pathname]);
 
   return null;
