264ab964dbd304ebd1b98fdc244943f277d32704
Stanislav Kozin <stas@staskozin.ru>
2020-08-24 04:02:55 +0300

    Move projects data to database

--- src/component/MainPage/Projects/Projects.jsx
+++ src/component/MainPage/Projects/Projects.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-
-import squares from './squares';
+import React, { useState, useEffect } from 'react';
 
 import './Projects.scss';
 import Square from './Square/Square';
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('https://staskozin.ru/api/get_projects.php')
+      .then(r => r.json())
+      .then(r => setProjects(r));
+  }, []);
+
   return (
+    projects.length !== 0 &&
     <>
       <div className="squares">
-        {squares.filter(square => !square.archived).map(square => <Square data={square} key={square.css_class} />)}
+        {projects.filter(project => !project.archived).map(project => <Square data={project} key={project.project_id} />)}
       </div>
       <h2>Архивные проекты</h2>
       <div className="squares">
-        {squares.filter(square => square.archived).map(square => <Square data={square} key={square.css_class} />)}
+        {projects.filter(project => project.archived).map(project => <Square data={project} key={project.project_id} />)}
       </div>
     </>
   )
